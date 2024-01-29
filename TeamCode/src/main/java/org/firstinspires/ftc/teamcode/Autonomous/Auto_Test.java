a41444dbad806f853f903f7d499543ecbcbd42b1
harukasuwabe <157557799+harukasuwabe@users.noreply.github.com>
2024-01-28 22:39:53 -0500

    Added road runner and OpenCV

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_Test.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Movement;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+//import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+public class Auto_Test {
+
+    }
+
+
