b23f4e5fab3771276df43b2183f1a6ddc4e3e789
harukasuwabe <157557799+harukasuwabe@users.noreply.github.com>
2024-02-11 16:55:47 -0500

    Merge remote-tracking branch 'lovelace/master'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HSV/PropThreshold.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HSV/PropThreshold.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.HSV;
 import android.graphics.Canvas;
 
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.teamcode.util.CustomTypes;
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -17,9 +18,10 @@ public class PropThreshold implements VisionProcessor {
     private Mat highMat = new Mat();
     private Mat lowMat = new Mat();
     private Mat finalMat = new Mat();
-    private static double threshold = 0.001;
+    private static double threshold = 0.01; //Maybe change this depending
 
     String outStr = "none"; //Set a default value in case vision does not work
+    CustomTypes.PropLocation propLocation;
     double leftPerc;
     double middlePerc;
 
@@ -31,13 +33,13 @@ public class PropThreshold implements VisionProcessor {
     public void init(int width, int height, CameraCalibration calibration) {
 
         this.LEFT_RECTANGLE = new Rect(
-                new Point(0,0),
-                new Point(width/3, height)
+                new Point(0, 0.3 * height),
+                new Point(0.222 * width, 0.6 * height)
         );
 
         this.MIDDLE_RECTANGLE = new Rect(
-                new Point(width / 3, height),
-                new Point(3 * width / 4, height)
+                new Point(0.3125 * width, 0.5 * height),
+                new Point(0.9 * width, height)
         );
     }
 
@@ -57,38 +59,38 @@ public class PropThreshold implements VisionProcessor {
             Scalar lowHSVRedLower = new Scalar(0, 100, 20);  //Beginning of Color Wheel
             Scalar lowHSVRedUpper = new Scalar(10, 255, 255);
 
-            Scalar redHSVRedLower = new Scalar(160, 100, 20); //Wraps around Color Wheel
-            Scalar highHSVRedUpper = new Scalar(180, 255, 255);
+            Scalar redHSVRedLower = new Scalar(169, 100, 20); //Wraps around Color Wheel
+            Scalar highHSVRedUpper = new Scalar(179, 255, 255);
 
             Core.inRange(testMat, lowHSVRedLower, lowHSVRedUpper, lowMat);
             Core.inRange(testMat, redHSVRedLower, highHSVRedUpper, highMat);
             Core.bitwise_or(lowMat, highMat, finalMat);
         } else {
-            Scalar blueHSVLower = new Scalar(85, 100, 20);
+            Scalar blueHSVLower = new Scalar(89, 100, 20);
             Scalar blueHSVUpper = new Scalar(140, 255, 255);
 
             Core.inRange(testMat, blueHSVLower, blueHSVUpper, finalMat);
         }
-        testMat.release();
-
-        lowMat.release();
-        highMat.release();
+//        testMat.release();
+//
+//        lowMat.release();
+//        highMat.release();
 
         double leftBox = Core.sumElems(finalMat.submat(LEFT_RECTANGLE)).val[0];
         double middleBox = Core.sumElems(finalMat.submat(MIDDLE_RECTANGLE)).val[0];
 
         this.leftPerc = leftBox / LEFT_RECTANGLE.area() / 255;
         this.middlePerc = middleBox / MIDDLE_RECTANGLE.area() / 255; //Makes value [0,1]
-        this.middlePerc *= 2;
+//        this.middlePerc *= 2;
 
         if(leftPerc > middlePerc
                 && leftPerc > threshold) {
-            outStr = "left";
+            propLocation = CustomTypes.PropLocation.LEFT;
         } else if (middlePerc > leftPerc
                 && middlePerc > threshold) {
-            outStr = "middle";
+            propLocation = CustomTypes.PropLocation.MIDDLE;
         } else {
-            outStr = "none";
+            propLocation = CustomTypes.PropLocation.RIGHT;
         }
 
 		/*This line should only be added in when you want to see your custom pipeline
@@ -113,7 +115,7 @@ public class PropThreshold implements VisionProcessor {
                 break;
         }
 
-        return frame;
+        return null;
 
 //		return null; //You do not return the original mat anymore, instead return null
 
